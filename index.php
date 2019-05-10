7107c26571d94aeb4920c374c14d665bbf5a18ce
Gintaras <gintaras.garsva@gmail.com>
2019-05-10 16:56:50 +0300

    finished files: index.php, about.php, pizza.php, style.css

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
-<?php
-function familyName($fname) {
-    echo "$fname Refsnes.<b><p>";
-}
+<!DOCTYPE html>
 
-familyName("Jani");
-familyName("Hege");
-familyName("Stale");
-familyName("Kai Jim");
-familyName("Borge");
-?>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+    <link rel="stylesheet" type="text/css" href="grid.css">
+    <title>PZ2A PIZZA</title>
+</head>
+<body>
+    <nav class="col-12" id="strickynav">
+        <a href="index.php">PZ2APICERIJA</a>
+        <a href="about.php" class="linksright">AbOuT</a>
+        <a href="pizza.php" class="linksright">PZ2ApiCa</a>
+    </nav>
+    <section>
+        <div>
+            <img src="https://slice.seriouseats.com/images/2012/08/animatedpizzagifs-deal-with-it.gif">
+        </div>
+    </section>
+</body>
+</html>
