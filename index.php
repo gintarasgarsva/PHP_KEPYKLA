dddf0e6758833510e5afb693ff66ed6a039e4fe2
Gintaras <gintaras.garsva@gmail.com>
2019-05-10 14:26:45 +0300

    starting repository

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+function familyName($fname) {
+    echo "$fname Refsnes.<b><p>";
+}
+
+familyName("Jani");
+familyName("Hege");
+familyName("Stale");
+familyName("Kai Jim");
+familyName("Borge");
+?>
